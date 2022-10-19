d815be24195620b84c9b01e462244edcd0eefd61
MohamedHassan010 <mohammed20191700522@cis.asu.edu.eg>
2022-10-19 07:51:10 +0200

    online musing

---
+++ ConsumeSpotifyWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using ConsumeSpotifyWebAPI.Models;
+using ConsumeSpotifyWebAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsumeSpotifyWebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISpotifyAccountService _spotifyAccountService;
+        private readonly IConfiguration _configuration;
+        private readonly ISpotifyService _spotifyService;
+
+        public HomeController(
+            ISpotifyAccountService spotifyAccountService,
+            IConfiguration configuration,
+            ISpotifyService spotifyService)
+        {
+            _spotifyAccountService = spotifyAccountService;
+            _configuration = configuration;
+            _spotifyService = spotifyService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var newReleases = await GetReleases();
+
+            return View(newReleases);
+        }
+
+        private async Task<IEnumerable<Release>> GetReleases()
+        {
+            try
+            {
+                var token = await _spotifyAccountService.GetToken("8139125d01db4b84b7ed41a28792088c",
+                 "a741e915caa24d11a9d89d0e8d7673a3");
+
+                var newReleases = await _spotifyService.GetNewReleases("US", 20, token);
+
+                return newReleases;
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex);
+
+                return Enumerable.Empty<Release>();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
